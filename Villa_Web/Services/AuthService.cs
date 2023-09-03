1eb7f033e94177396eaebb94b9bdfb98dbf5c75e
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-03 17:24:24 +0430

    09/03/2023 Login And Registration Page

---
+++ Villa_Web/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using Villa_Utility;
+using Villa_Web.Models;
+using Villa_Web.Models.DTO;
+using Villa_Web.Services.IServices;
+
+namespace Villa_Web.Services
+{
+	public class AuthService : BaseService, IAuthService
+    {
+       private readonly IHttpClientFactory _httpClientFactory;
+		private string villaUrl;
+
+	public AuthService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+	{
+		_httpClientFactory = httpClientFactory;
+		villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+	}
+
+		public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				APIType = SD.ApiType.POST,
+				Data = obj,
+				Url = villaUrl + "/api/UsersAuth/login"
+			});
+		}
+
+		public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				APIType = SD.ApiType.POST,
+				Data = obj,
+				Url = villaUrl + "/api/UsersAuth/register"
+			});
+		}
+	}
+}
